c368bbc238936053213cb55f56a40a3a44ea0fea
Lignum <me@lignum.pw>
2017-10-14 22:07:04 +0200

    Allow passing file to markov-web

--- src/web/main.rs
+++ src/web/main.rs
@@ -10,22 +10,25 @@ use rocket::State;
 use markov_core::chain;
 use markov_core::graph::Graph;
 
-struct Context {
-    chain: Graph<chain::ChainNode, f64>
-}
-
 #[get("/")]
-fn index(ctx: State<Context>) -> String {
+fn index(chain: State<Graph<chain::ChainNode, f64>>) -> String {
     let mut rng = rand::thread_rng();
-    chain::generate_sentence(&mut rng, &ctx.chain)
+    chain::generate_sentence(&mut rng, &chain)
 }
 
 fn main() {
-    let chain = markov_core::chain::deserialise_chain("test.mkv").expect("Failed to deserialise markov chain!");
+    let args: Vec<_> = std::env::args().collect();
+    let prog_name = &args[0];
 
-    let ctx = Context { chain };
+    if args.len() < 2 {
+        eprintln!("Usage: {} [.mkv file]", prog_name);
+        return;
+    }
+
+    let filename = &args[1];
+    let chain = markov_core::chain::deserialise_chain(filename).expect("Failed to deserialise markov chain!");
 
     rocket::ignite()
-        .manage(ctx)
+        .manage(chain)
         .mount("/", routes![index]).launch();
 }
